9848cd6b773e6517a45857947b488b0a1fd0d074
BobNarwhalKing <danceflurry4366@gmail.com>
2020-01-28 15:04:09 -0600

    Hangman Word Generator started

---
+++ wordGenerator.py
@@ -0,0 +1,21 @@
+import random
+# words = open("words.txt", 'r') 
+# returnedWords = words.read()
+# print(returnedWords)
+# file1 = open("words.txt","w") 
+# L = ["Banana\n","Horse\n","Super\n","Puzzle\n","Dictionary\n","Free\n","Cellular\n","Kurtis\n","Brianna\n","Netflix\n"]   
+# file1.writelines(L) 
+# file1.close() #to change file acc
+
+
+def getHangmanWord(minCharcters):
+    wordsProcessed = 0
+    currWord = None
+    with open('words.txt','r') as f:
+        for word in f:
+            if '(' in word or ')' in word:
+                continue
+            wordsProcessed += 1
+            if random.randint(1, wordsProcessed) == 1:
+                currWord = word
+    return (currWord)
