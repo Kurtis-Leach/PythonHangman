7171c4f2194f0650e3d52df0203dfdae7d856719
BobNarwhalKing <danceflurry4366@gmail.com>
2020-01-30 20:59:57 -0600

    Error handling for user input

--- hangman.py
+++ hangman.py
@@ -2,21 +2,27 @@ from wordGenerator import getHangmanWord
 
 def hangman():
     print('Welcome to Hangman! \nThe rules are simple \nGuess the word \nYou only get so many chances before you lose \nGood Luck!')
-    maxErrors = input(' Pick a number [1-10] for the amount of mistakes you may have: ')
-    maxError = numberInputChecker(maxErrors)
-    minCharacters = input('Pick a number [1-10] for minimum word length: ')
-    minCharacter = numberInputChecker(minCharacters)
+    while True:
+        try: 
+            maxErrors = input(' Pick a number [1-10] for the amount of mistakes you may have: ')
+            maxError = numberInputChecker(maxErrors)
+            minCharacters = input('Pick a number [1-10] for minimum word length: ')
+            minCharacter = numberInputChecker(minCharacters)
+            break
+        except (NameError, SyntaxError):
+            print ('You must choose a number between 1 and 10 not a letter')
+
+    
     print (maxError)
     print (minCharacter)
 
 def numberInputChecker(inputRecieved):
-    inputRecieved = int(inputRecieved)
     if inputRecieved < 1 or inputRecieved > 10:
         print('Must be between 1 and 10')
-        inputRecieved = input(' Pick a number [1-10] for the amount of mistakes you may have: ')
+        inputRecieved = input('Pick a number [1-10] for the amount of mistakes you may have: ')
         inputRecieved = numberInputChecker(inputRecieved)
         return (inputRecieved)
     else:
         return (inputRecieved)
 
-# hangman()
+hangman()
